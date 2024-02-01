109445016e31094ff35785523388917e6f183d71
Disease <83523587+CPScript@users.noreply.github.com>
2024-02-01 00:00:31 -0500

    Create panic.sh

---
+++ Hardware/Kernel/kernel-panic/script/panic.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Check if the script is being run as root
+if [ "$(id -u)" != "0" ]; then
+   echo "Must be run as root, type `su`" 1>&2
+   exit 1
+fi
+
+# Security
+read -p "Are you sure you want to trigger a kernel panic? This will crash the system. (y/n) " response
+if [[ $response =~ ^([yY][eE][sS]|[yY])$ ]]
+then
+    # Trigger a kernel panic
+    echo 'c' | tee -a /proc/sysrq-trigger
+else
+    echo "aborted."
+    exit 0
+fi
